8e3ca30778b2c1ea7a7e9527b2ec33227268f95a
César Morgan <xds_xps_ivx@hotmail.com>
2023-04-06 18:55:43 -0600

    New: Filterable discovery engine

---
+++ src/ServicePool.Tests/FilterableDiscoveryEngineTests.cs
@@ -0,0 +1,92 @@
+﻿// FilterableDiscoveryEngineTests.cs
+//
+// This file is part of ServicePool
+//
+// Author(s):
+//      César Andrés Morgan <xds_xps_ivx@hotmail.com>
+//
+// Released under the MIT License (MIT)
+// Copyright © 2011 - 2022 César Andrés Morgan
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the “Software”), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma warning disable CS1591
+
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace TheXDS.ServicePool.Tests;
+
+public class FilterableDiscoveryEngineTests
+{
+    [Test]
+    public void Engine_filters_out_unwanted_types_with_params_ctor()
+    {
+        var e = new FilterableDiscoveryEngine(typeof(InvalidOperationException), typeof(StackOverflowException));
+        Assert.That(e.Discover(typeof(Exception)), Does.Not.Contain(typeof(InvalidOperationException)));
+        Assert.That(e.Discover(typeof(Exception)), Does.Not.Contain(typeof(StackOverflowException)));
+        Assert.That(e.Discover(typeof(Exception)), Contains.Item(typeof(ArgumentException)));
+        Assert.That(e.Discover(typeof(Exception)), Contains.Item(typeof(NullReferenceException)));
+    }
+
+    [Test]
+    public void Engine_filters_out_unwanted_types_with_enumerable_ctor()
+    {
+        var e = new FilterableDiscoveryEngine(new[] { typeof(InvalidOperationException), typeof(StackOverflowException) }.AsEnumerable());
+        Assert.That(e.Discover(typeof(Exception)), Does.Not.Contain(typeof(InvalidOperationException)));
+        Assert.That(e.Discover(typeof(Exception)), Does.Not.Contain(typeof(StackOverflowException)));
+        Assert.That(e.Discover(typeof(Exception)), Contains.Item(typeof(ArgumentException)));
+        Assert.That(e.Discover(typeof(Exception)), Contains.Item(typeof(NullReferenceException)));
+    }
+
+    [Test]
+    public void Engine_filters_out_unwanted_types_with_exclusion_registration()
+    {
+        var e = new FilterableDiscoveryEngine()
+        {
+            Exclusions =
+            {
+                typeof(InvalidOperationException),
+                typeof(StackOverflowException),
+            }
+        };
+        Assert.That(e.Discover(typeof(Exception)), Does.Not.Contain(typeof(InvalidOperationException)));
+        Assert.That(e.Discover(typeof(Exception)), Does.Not.Contain(typeof(StackOverflowException)));
+        Assert.That(e.Discover(typeof(Exception)), Contains.Item(typeof(ArgumentException)));
+        Assert.That(e.Discover(typeof(Exception)), Contains.Item(typeof(NullReferenceException)));
+    }
+
+    [Test]
+    public void Engine_contains_exclusions()
+    {
+        var e = new FilterableDiscoveryEngine()
+        {
+            Exclusions =
+            {
+                typeof(InvalidOperationException),
+                typeof(StackOverflowException),
+            }
+        };
+        Assert.That(e.Exclusions, Contains.Item(typeof(InvalidOperationException)));
+        Assert.That(e.Exclusions, Contains.Item(typeof(StackOverflowException)));
+        Assert.That(e.Exclusions, Does.Not.Contain(typeof(ArgumentException)));
+        Assert.That(e.Exclusions, Does.Not.Contain(typeof(NullReferenceException)));
+    }
+}
---
+++ src/ServicePool/FilterableDiscoveryEngine.cs
@@ -0,0 +1,80 @@
+﻿// FilterableDiscoveryEngine.cs
+//
+// This file is part of ServicePool
+//
+// Author(s):
+//      César Andrés Morgan <xds_xps_ivx@hotmail.com>
+//
+// Released under the MIT License (MIT)
+// Copyright © 2011 - 2022 César Andrés Morgan
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the “Software”), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheXDS.ServicePool;
+
+/// <summary>
+/// Implements a <see cref="IDiscoveryEngine"/> that searches for types in the current <see cref="AppDomain"/>, but excludes the types explicitly added to a list of exclusions.
+/// </summary>
+public class FilterableDiscoveryEngine : IDiscoveryEngine
+{
+    private readonly HashSet<Type> _exclusions;
+    private readonly IDiscoveryEngine _engine = new DefaultDiscoveryEngine();
+
+    /// <summary>
+    /// Initializes a new instance of the
+    /// <see cref="FilterableDiscoveryEngine"/> class.
+    /// </summary>
+    public FilterableDiscoveryEngine() : this((IEnumerable<Type>?)null)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the
+    /// <see cref="FilterableDiscoveryEngine"/> class.
+    /// </summary>
+    /// <param name="exclusions">
+    /// Types to be excluded from the search when discovering types.
+    /// </param>
+    public FilterableDiscoveryEngine(IEnumerable<Type>? exclusions)
+    {
+        _exclusions = new(exclusions ?? Array.Empty<Type>());
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the
+    /// <see cref="FilterableDiscoveryEngine"/> class.
+    /// </summary>
+    /// <param name="exclusions">
+    /// Types to be excluded from the search when discovering types.
+    /// </param>
+    public FilterableDiscoveryEngine(params Type[] exclusions) : this(exclusions.AsEnumerable()) { }
+
+    /// <summary>
+    /// Gets a collection of types that should be omitted from being
+    /// automatically discovered.
+    /// </summary>
+    public ICollection<Type> Exclusions => _exclusions;
+
+    /// <inheritdoc/>
+    public IEnumerable<Type> Discover(Type t) => _engine.Discover(t).Where(p => !_exclusions.Contains(p));
+}
